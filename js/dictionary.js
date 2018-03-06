171d92fed9a9f457a3542cea0fd5b62aff137065
anusaran <anusaran.site@gmail.com>
2018-03-06 16:06:00 +0530

    Added Translation Code for Selections

---
+++ js/dictionary.js
@@ -0,0 +1,71 @@
+var dict = {
+    "Anusaran": {
+        en: "Anusaran",
+        hn: "निमंत्रण",
+        mr: "निमंत्रण"
+    },
+    "English Books": {
+        hn: "Hindi Books",
+    },
+    "God's Smuggler": {
+        hn: "भगवान का तस्कर",
+        bn: "God's Smuggler in Bengali",
+        en: "God's Smuggler in English",
+        mr: "देवाचा चोर",
+        gu: "God's Smuggler in Gujarati",
+        pa: "God's Smuggler in Punjabi",
+        or: "God's Smuggler in Odia"
+    },
+    "gods-smuggler-description": {
+        en: "Readers are gripped from the first page by the harrowing account of a young man who risked his life to smuggle Bibles through the borders of closed nations. Now, sixty years after Brother Andrew first prayed for God's miracle protection, this expanded edition of a classic work encourages new readers to meet this remarkable man and his mission for the first time.",
+        hn: "पाठकों को एक जवान आदमी के दु: खद खाते से पहला पेज से दबदबा जाता है जिसने बंद देशों की सीमाओं के माध्यम से बाइबल को तस्करी करने के लिए अपना जीवन व्यतीत किया। अब, भाई एंड्रयू ने पहली बार भगवान के चमत्कार की रक्षा के लिए साठ साल बाद, एक क्लासिक कार्य के इस विस्तारित संस्करण में नए पाठकों को पहली बार इस उल्लेखनीय व्यक्ति और उनके मिशन को पूरा करने के लिए प्रोत्साहित किया गया।"
+    },
+    "Pastor's Manual": {
+        en: "Pastor's Manual",
+        hn: "पादरी के मैनुअल"
+    },
+    "Teacher's Module 1": {
+        mr: "Teacher's Module 1 in Marathi",
+        bn: "Teacher's Module 1 in Bengali",
+        ne: "Teacher's Module 1 in Nepali",
+        or: "Teacher's Module 1 in Odia",
+        pa: "Teacher's Module 1 in Punjabi",
+        te: "Teacher's Module 1 in Telugu",
+        hn: "Teacher's Module 1 in Hindi"
+    },
+    "Teacher's Module 2": {
+        mr: "Teacher's Module 2 in Marathi",
+        bn: "Teacher's Module 2 in Bengali",
+        ne: "Teacher's Module 2 in Nepali",
+        or: "Teacher's Module 2 in Odia",
+        pa: "Teacher's Module 2 in Punjabi",
+        te: "Teacher's Module 2 in Telugu",
+        hn: "Teacher's Module 2 in Hindi"
+    },
+    "Student's Module 1": {
+        mr: "Student's Module 1 in Marathi",
+        bn: "Student's Module 1 in Bengali",
+        ne: "Student's Module 1 in Nepali",
+        or: "Student's Module 1 in Odia",
+        pa: "Student's Module 1 in Punjabi",
+        te: "Student's Module 1 in Telugu",
+        hn: "Student's Module 1 in Hindi"
+    },
+    "Student's Module 2": {
+        mr: "Student's Module 2 in Marathi",
+        bn: "Student's Module 2 in Bengali",
+        ne: "Student's Module 2 in Nepali",
+        or: "Student's Module 2 in Odia",
+        pa: "Student's Module 2 in Punjabi",
+        te: "Student's Module 2 in Telugu",
+        hn: "Student's Module 2 in Hindi"
+    },
+    "Faith that Endures": {
+        hn: "विश्वास है कि समर्थन करता है",
+        en: "Faith that Endures"
+    },
+    "download": {
+        hn: "डाउनलोड",
+        en: "download"
+    }
+}
